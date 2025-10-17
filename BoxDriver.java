a156d2177e1dcd91ce8e44467ae16cf4a163843d
LakshP77 <lspvsp@gmail.com>
2025-10-17 19:43:16 +0000

    comments done pt 1 done

--- BoxDriver.java
+++ BoxDriver.java
@@ -1,3 +1,27 @@
+/* 
+
+1. The variable watch window displays all variables currently in scope, along with their names, types, and current values.
+It helps you see how data changes during execution, making it easier to find logic or runtime errors.
+
+2. The method call stack shows the sequence of active method calls that led to the current point in the program.
+It lets you trace which methods were called and in what order, helping you understand the program’s flow.
+
+3. 
+a. Step (Step Over)
+Step executes the current line of code and moves to the next line in the same method.
+If the line calls another method, it runs that method completely without entering it.
+
+b. Step Into
+Step Into executes the current line of code and, if it calls another method, goes inside that method to debug it line by line.
+It’s useful when you want to closely examine what happens within a called method.
+
+c. Continue (Resume)
+Continue runs the program normally from the current breakpoint until another breakpoint or the end of the program.
+It’s used when you’re done stepping through code and want to let the program proceed on its own.
+*/
+
+
+
 public class BoxDriver {
 
     public static void main(String[] args) {
